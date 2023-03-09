ee49fc65801f1cc1f9f955f73e87cc7e1f78e1b5
Honghong <jihong700@naver.com>
2023-03-09 18:13:22 +0900

    project

---
+++ back/src/dao/indexDao.js
@@ -0,0 +1,25 @@
+const { pool } = require("../../config/database");
+
+exports.selectRestaurants = async function (connection, category) {
+  const selectAllRestaurantsQuery = `SELECT title, address, category, videoUrl FROM Restaurants where status = 'A';`;
+  const selectCategorizedRestaurantsQuery = `SELECT title, address, category, videoUrl FROM Restaurants where status = 'A' and category = ?;`;
+
+  const Params = [category];
+
+  const Query = category
+    ? selectCategorizedRestaurantsQuery
+    : selectAllRestaurantsQuery;
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+// exports.exampleDao = async function (connection, params) {
+//   const Query = ``;
+//   const Params = [];
+
+//   const rows = await connection.query(Query, Params);
+
+//   return rows;
+// };
